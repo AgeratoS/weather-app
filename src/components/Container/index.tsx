4785b7c2389825b227d4142c50739e9ac23d76b5
Alexey Matveev <leshka99514@gmail.com>
2021-07-04 19:13:43 +0300

    added component Container

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { breakpoints } from "../../theme/breakpoints";
+
+export const Container = styled.div`
+  max-width: ${breakpoints.lg};
+  margin: 0 auto;
+`
---
+++ src/theme/breakpoints.ts
@@ -0,0 +1,7 @@
+export const breakpoints = {
+  "xs": "320px",
+  "sm": "480px",
+  "md": "640px",
+  "lg": "960px",
+  "xl": "1140px"
+}
